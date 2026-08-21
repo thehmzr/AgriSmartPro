a76f24aff5d00094e8283cb8695c58287fe7a09a
Hamza <hmza.44123@gmail.com>
2026-08-21 13:30:30 +0300

    add flask app with the three prediction routes

---
+++ backend/app.py
@@ -0,0 +1,132 @@
+import app
+from flask import Flask, render_template, request
+from markupsafe import Markup
+from utils.fertilizer import fertilizer_dic
+import pandas as pd
+import numpy as np
+import pickle
+import sklearn
+
+
+print(sklearn.__version__)
+#loading models
+dtr = pickle.load(open('dtr.pkl','rb'))
+preprocessor = pickle.load(open('preprocessor .pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+sc = pickle.load(open('standscaler.pkl','rb'))
+ms = pickle.load(open('minmaxscaler.pkl','rb'))
+
+# flask app
+app = Flask(__name__)
+
+
+@app.route('/reset')
+def reset():
+    return render_template('reset.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    return render_template('fertilizer.html')
+@ app.route('/')
+def demo():
+    return render_template('demo.html')
+@ app.route('/Dashboard')
+def dashboard():
+    return render_template('Dashboard.html')
+@ app.route('/test')
+def test():
+    return render_template('test.html')
+
+@ app.route('/StockManagement')
+def stock():
+    return render_template('stock.html')
+@app.route("/predict1", methods=['POST'])
+def predict1():
+    if request.method == 'POST':
+        Year = 2024
+        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = request.form['pesticides_tonnes']
+        avg_temp = request.form['avg_temp']
+        Area = request.form['Area']
+        Item = request.form['Item']
+
+        features = np.array([[Year, average_rain_fall_mm_per_year, pesticides_tonnes, avg_temp, Area, Item]], dtype=object)
+        transformed_features = preprocessor.transform(features)
+        prediction = dtr.predict(transformed_features).reshape(1, -1)
+
+        return render_template('index.html', prediction=prediction)
+
+@app.route("/predict2", methods=['POST'])
+def predict2():
+    N = request.form['Nitrogen']
+    P = request.form['Phosporus']
+    K = request.form['Potassium']
+    temp = request.form['Temperature']
+    humidity = request.form['Humidity']
+    ph = request.form['Ph']
+    rainfall = request.form['Rainfall']
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+
+    scaled_features = ms.transform(single_pred)
+    final_features = sc.transform(scaled_features)
+    prediction = model.predict(final_features)
+
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+    if prediction[0] in crop_dict:
+        crop = crop_dict[prediction[0]]
+        result = "{} is the best crop to be cultivated right there".format(crop)
+    else:
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
+    return render_template('index1.html', result=result)
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}  #ensure values are non-negative
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer.html', recommendation=response)
+if __name__ == "__main__":
+    app.run(debug=True)
